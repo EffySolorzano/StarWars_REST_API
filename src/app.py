a776f643e499bcaa34a9bf93dd33132000ce4194
EffySolorzano <solorzano.steph@gmail.com>
2023-04-01 03:57:14 +0000

    1stupdate_people

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People
 #from models import Person
 
 app = Flask(__name__)
@@ -38,12 +38,114 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    users = User.query.all()
+    users = list(map(lambda item: item.serialize(), users))
+    print(users)
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(users), 200 
 
-    return jsonify(response_body), 200
+@app.route('/register', methods=['POST'])
+def register_user():
+    #recibir el body en json, des-jsonificarlo y almacenarlo en la variable body
+    body = request.get_json() #request.json() pero hay que importar request y json
+
+    #ordernar cada uno de los campos recibidos
+    email = body["email"]
+    name = body["name"]
+    password = body["password"]
+    is_active = body["is_active"]
+
+    #validaciones
+    if body is None:
+        raise APIException("You need to specify the request body as json object", status_code=400)
+    if "email" not in body:
+        raise APIException("You need to specify the email", status_code=400)
+
+    #creada la clase User en la variable new_user
+    new_user = User(email=email, name=name, password=password, is_active=is_active)
+
+    #comitear la sesión
+    db.session.add(new_user) #agregamos el nuevo usuario a la base de datos
+    db.session.commit() #guardamos los cambios en la base de datos
+
+    return jsonify({"mensaje":"Usuario creado correctamente"}), 201 
+
+@app.route('/get-user/<int:id>', methods=['GET'])
+def get_specific_user(id):
+    user = User.query.get(id)    
+  
+    return jsonify(user.serialize()), 200
+
+@app.route('/get-user', methods=['POST'])
+def get_specific_user2():
+    body = request.get_json()   
+    id = body["id"]
+
+    user = User.query.get(id)   
+  
+    return jsonify(user.serialize()), 200
+
+@app.route('/get-user', methods=['DELETE'])
+def delete_specific_user():
+    body = request.get_json()   
+    id = body["id"]
+
+    user = User.query.get(id) 
+
+    db.session.delete(user)
+    db.session.commit()  
+  
+    return jsonify("Usuario borrado"), 200
+
+@app.route('/get-user', methods=['PUT'])
+def edit_user():
+    body = request.get_json()   
+    id = body["id"]
+    name = body["name"]
+
+    user = User.query.get(id)   
+    user.name = name #modifique el nombre del usuario
+
+    db.session.commit()
+  
+    return jsonify(user.serialize()), 200
+
+@app.route('/add', methods=[ 'POST' ])
+def add_people():
+    body = request.get_json()
+    name = body["name"]
+    height = body["height"]
+    mass = body["mass"]
+    hair_color = body["hair_color"]
+
+    if body is None:
+        raise APIException("You need to specify the request body as json object", status_code=400)
+    if "name" not in body:
+        raise APIException("You need to specify the name", status_code=400)
+    if  "height" not in body:
+        raise APIException("You need to specify the height", status_code=400)
+    if  "mass" not in body:
+        raise APIException("You need to specify the mass", status_code=400)
+    if  "hair_color" not in body:
+        raise APIException("You need to specify the hair_color", status_code=400)
+
+    # create a new People object with the data provided
+    new_person = People(name=name, height=height, mass=mass, hair_color=hair_color)
+
+    # add the new person to the database and commit changes
+    db.session.add(new_person)
+    db.session.commit()
+
+    return jsonify({"message": "New character added"}), 201
+
+@app.route('/get-person/<string:name>', methods = [ 'GET' ])
+def get_specific_person(name):
+    person = People.query.filter_by(name=name).first()
+    if person:
+        return jsonify(person.serialize()), 200
+    else:
+        raise APIException('Person not found', status_code=404)    
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,13 +7,32 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    height = db.Column(db.String(80), unique=False, nullable=False)
+    mass = db.Column(db.String(80), unique=False, nullable=False)
+    hair_color = db.Column(db.String(80), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            # do not serialize the password, its a security breach
+        }        
